22ffbc550fdb07b8d5ebc2dc152942be8a854169
Lukas Werling <ufdnn@student.kit.edu>
2013-05-13 22:04:07 +0200

    Add grey histogram tests

---
+++ src/test/java/edu/kit/ipd/jmjrst/deduplicator/GreyHistogramComparatorTest.java
@@ -0,0 +1,114 @@
+package edu.kit.ipd.jmjrst.deduplicator;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the histogram comparator with a couple of sample images.
+ */
+public class GreyHistogramComparatorTest {
+	
+	/**
+	 * Test comparisons.
+	 */
+	GreyHistogramComparator all, bw, grey;
+
+	/**
+	 * Loads the sample images with some grouping.
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		File white = new File("src/test/resources/white.png");
+		File black = new File("src/test/resources/black.png");
+		File gradient = new File("src/test/resources/gradient.png");
+		File color = new File("src/test/resources/colors.png");
+		
+		List<File> allFiles = Arrays.asList(new File[] { white, black, gradient, color });
+		List<File> bwFiles = Arrays.asList(new File[] { white, black });
+		List<File> greyFiles = Arrays.asList(new File[] { white, black, gradient });
+		
+		all = new GreyHistogramComparator();
+		all.setFiles(allFiles);
+		bw = new GreyHistogramComparator();
+		bw.setFiles(bwFiles);
+		grey = new GreyHistogramComparator();
+		grey.setFiles(greyFiles);
+	}
+
+	/**
+	 * Tests the dimension of the result matrix.
+	 */
+	@Test
+	public void testGetSimilaritiesBWDimension() {
+		float[][] sim = bw.getSimilarities();
+		assertEquals(2, sim.length);
+		assertEquals(2, sim[0].length);
+	}
+
+	/**
+	 * Tests the dimension of the result matrix.
+	 */
+	@Test
+	public void testGetSimilaritiesGreyDimension() {
+		float[][] sim = grey.getSimilarities();
+		assertEquals(3, sim.length);
+		assertEquals(3, sim[0].length);
+	}
+
+	/**
+	 * Tests the dimension of the result matrix.
+	 */
+	@Test
+	public void testGetSimilaritiesAllDimension() {
+		float[][] sim = all.getSimilarities();
+		assertEquals(4, sim.length);
+		assertEquals(4, sim[0].length);
+	}
+
+	/**
+	 * Tests the contents of the result matrix.
+	 */
+	@Test
+	public void testGetSimilaritiesBW() {
+		float[][] sim = bw.getSimilarities();
+		assertEquals(1.0, sim[0][0], 0.001f);
+		assertEquals(1.0, sim[1][1], 0.001f);
+		assertEquals(0.0, sim[1][0], 0.001f);
+		assertEquals(0.0, sim[0][1], 0.001f);
+	}
+
+	/**
+	 * Tests the contents of the result matrix.
+	 */
+	@Test
+	public void testGetSimilaritiesAllIdentity() {
+		float[][] sim = all.getSimilarities();
+		assertEquals(1.0, sim[0][0], 0.001f);
+		assertEquals(1.0, sim[1][1], 0.001f);
+		assertEquals(1.0, sim[2][2], 0.001f);
+		assertEquals(1.0, sim[3][3], 0.001f);
+	}
+
+	/**
+	 * Tests the contents of the result matrix.
+	 * 
+	 * The grey gradient should roughly have a similarity of 0.5 to the pure images.
+	 */
+	@Test
+	public void testGetSimilaritiesGrey() {
+		float[][] sim = grey.getSimilarities();
+		assertEquals(0.5, sim[0][2], 0.1f);
+		assertEquals(0.5, sim[2][0], 0.1f);
+		assertEquals(0.5, sim[1][2], 0.1f);
+		assertEquals(0.5, sim[2][1], 0.1f);
+	}
+
+}
---
+++ src/test/java/edu/kit/ipd/jmjrst/deduplicator/GreyHistogramTest.java
@@ -0,0 +1,86 @@
+package edu.kit.ipd.jmjrst.deduplicator;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the grey histogram calculator.
+ *
+ */
+public class GreyHistogramTest {
+	
+	/**
+	 * Test files.
+	 */
+	GreyHistogram white, black, gradient, color;
+
+	/**
+	 * Load all test image files.
+	 * @throws Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		white = new GreyHistogram();
+		white.buildFrom(new File("src/test/resources/white.png"));
+		black = new GreyHistogram();
+		black.buildFrom(new File("src/test/resources/black.png"));
+		gradient = new GreyHistogram();
+		gradient.buildFrom(new File("src/test/resources/gradient.png"));
+		color = new GreyHistogram();
+		color.buildFrom(new File("src/test/resources/colors.png"));
+	}
+
+	/**
+	 * Simple case: fully white image.
+	 */
+	@Test
+	public void testGetHistogramWhite() {
+		assertEquals(1.0f, white.getHistogram()[255], 0.001f);
+		for (int i = 254; i >= 0; i--) {
+			assertEquals(0.0f, white.getHistogram()[i], 0.001f);
+		}
+	}
+
+	/**
+	 * Simple case: fully black image.
+	 */
+	@Test
+	public void testGetHistogramBlack() {
+		assertEquals(1.0f, black.getHistogram()[0], 0.001f);
+		for (int i = 255; i > 0; i--) {
+			assertEquals(0.0f, black.getHistogram()[i], 0.001f);
+		}
+	}
+
+	/**
+	 * Check the even grey distribution of the gradient.
+	 */
+	@Test
+	public void testGetHistogramGradient() {
+		float sum = 0;
+		for (int i = 255; i >= 0; i--) {
+			sum += gradient.getHistogram()[i];
+			// All values should roughly be included.
+			assertEquals(0.01f, gradient.getHistogram()[i], 0.01f);
+		}
+		assertEquals(1.0f, sum, 0.001f);
+	}
+
+	/**
+	 * Check whether the histogram value sum of the color image equals 1.
+	 */
+	@Test
+	public void testGetHistogramColor() {
+		float sum = 0;
+		for (int i = 255; i >= 0; i--) {
+			sum += color.getHistogram()[i];
+		}
+		assertEquals(1.0f, sum, 0.001f);
+	}
+
+}
