3028906db8da7ceed80b6cc1e8a442e6853eaf0d
Lukas Werling <ufdnn@student.kit.edu>
2013-05-27 17:57:34 +0200

    Adjust the Linkage classes to the new interface

--- src/main/java/edu/kit/ipd/jmjrst/deduplicator/cluster/AvgLinkage.java
+++ src/main/java/edu/kit/ipd/jmjrst/deduplicator/cluster/AvgLinkage.java
@@ -1,11 +1,17 @@
 package edu.kit.ipd.jmjrst.deduplicator.cluster;
 
-public class AvgLinkage implements LinkageMethod {
+/**
+ * Berechnet den Verbindungsgrad aus dem Mittel der vorigen Verbindungsgrade.
+ *
+ */
+public class AvgLinkage extends AbstractLinkage {
 
 	@Override
 	public float getLinkage(Cluster newCluster, Cluster existingCluster) {
-		// TODO Auto-generated method stub
-		return 0;
+		float left = getLeafSim(newCluster.getLeft().getFileIndex(), existingCluster.getFileIndex());
+		float right = getLeafSim(newCluster.getRight().getFileIndex(), existingCluster.getFileIndex());
+		
+		return 0.5f * left + 0.5f * right;
 	}
 
 }
--- src/main/java/edu/kit/ipd/jmjrst/deduplicator/cluster/CompleteLinkage.java
+++ src/main/java/edu/kit/ipd/jmjrst/deduplicator/cluster/CompleteLinkage.java
@@ -4,23 +4,14 @@ package edu.kit.ipd.jmjrst.deduplicator.cluster;
  * Berechnet den Verbindungsgrad anhand des Minimums der Ähnlichkeit aller Kindelemente der Cluster.
  *
  */
-public class CompleteLinkage implements LinkageMethod {
-
+public class CompleteLinkage extends AbstractLinkage {
+	
 	@Override
 	public float getLinkage(Cluster newCluster, Cluster existingCluster) {
-		if (newCluster.getId() == -1) {
-				return Math.min(
-						getLinkage(newCluster.getLeft(), existingCluster),
-						getLinkage(newCluster.getRight(), existingCluster));
-		} else {
-			if (existingCluster.getId() == -1) {
-				return Math.min(
-						getLinkage(newCluster, existingCluster.getLeft()),
-						getLinkage(newCluster, existingCluster.getRight()));
-			} else {
-				return newCluster.getDistance(existingCluster.getId());
-			}
-		}
+		float left = getLeafSim(newCluster.getLeft().getFileIndex(), existingCluster.getFileIndex());
+		float right = getLeafSim(newCluster.getRight().getFileIndex(), existingCluster.getFileIndex());
+		
+		return 0.5f * left + 0.5f * right - 0.5f * Math.abs(left - right);
 	}
 
 }
