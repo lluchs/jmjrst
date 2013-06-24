5769bee1b66aee5d08caf0b46dd8feb3c2d18da1
Lukas Werling <ufdnn@student.kit.edu>
2013-06-24 17:25:38 +0200

    Add top controls

--- src/main/java/edu/kit/ipd/jmjrst/view/ClusterDialog.java
+++ src/main/java/edu/kit/ipd/jmjrst/view/ClusterDialog.java
@@ -1,8 +1,14 @@
 package edu.kit.ipd.jmjrst.view;
 
+import java.awt.BorderLayout;
 import java.awt.HeadlessException;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSlider;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerNumberModel;
 
 import org.jis.Main;
 
@@ -14,6 +20,10 @@ import org.jis.Main;
 public class ClusterDialog extends JDialog {
 	private static final long serialVersionUID = 5875401850092115129L;
 	private Main m;
+	
+	private JSlider similarityThresholdSlider;
+	private JSpinner edgeQualityWeight;
+	private JSpinner megapixelsQualityWeight;
 
 	/**
 	 * @param m
@@ -23,8 +33,26 @@ public class ClusterDialog extends JDialog {
 	public ClusterDialog(Main m) throws HeadlessException {
 		super(m, m.mes.getString("Menu.17"), true);
 		this.m = m;
+		
+		// Create the upper panel for controls.
+		JPanel controls = new JPanel();
+		
+		controls.add(new JLabel("Ähnlichkeitsgrenzwert"));
+		similarityThresholdSlider = new JSlider(0, 100, 50);
+		controls.add(similarityThresholdSlider);
+		
+		controls.add(new JLabel("Gewicht der Auflösung"));
+		megapixelsQualityWeight = new JSpinner(new SpinnerNumberModel(50, 0, 100, 1));
+		controls.add(megapixelsQualityWeight);
 
-		setVisible(true);
+		controls.add(new JLabel("Gewicht der Kantensuche"));
+		edgeQualityWeight = new JSpinner(new SpinnerNumberModel(50, 0, 100, 1));
+		controls.add(edgeQualityWeight);
+		
+		this.add(controls, BorderLayout.NORTH);
+
+		this.pack();
+		this.setVisible(true);
 	}
 	
 	
